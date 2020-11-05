63d863549005fed853db2edb40a8ab71be48324e
luci49 <72394220+luci49@users.noreply.github.com>
2020-11-05 11:31:06 +0100

    Datentypen Switch case
    float num 2;
    case '+' :
    			ans = num1 + num2;
    			break;
            case '-' :
    			ans = num1 - num2;
    			break;
            case '*' :
    			ans = num1 * num2;
    			break;
            case '/' :
    			ans = num1 / num2;
    			break;

--- calculator.c
+++ calculator.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 
 /**
 * Simple calculator
@@ -6,7 +8,7 @@
 int main(){
 
 	float num1;
-	int num2;
+	float num2;
 	float ans;
 	char op;
 	printf("\nEnter an operator (+,-,/,*): \n");
@@ -15,15 +17,25 @@ int main(){
 	printf("\nEnter two numbers: ");
 	scanf("%f",&num1);
 	scanf("%f",&num2);
-	
+
 	switch(op){
-		case '+' : 
+		case '+' :
 			ans = num1 + num2;
 			break;
+        case '-' :
+			ans = num1 - num2;
+			break;
+        case '*' :
+			ans = num1 * num2;
+			break;
+        case '/' :
+			ans = num1 / num2;
+			break;
+
 		default:
 			printf("Enter a correct operator!");
 			return 1;
-	
+
 	}
 	printf("\n The result is as follows:\n");
 	printf("%f %c %f = %f",num1, op ,num2,ans);
